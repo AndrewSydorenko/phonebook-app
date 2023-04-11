fe38dd055fedd69fba631cb9fbec33cbd188cacd
Sam <98640251+AndrewSydorenko@users.noreply.github.com>
2023-04-11 19:32:05 +0300

    ffddd

--- src/redux/contacts/selectors.jsx
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-export const selectContacts = state => state.phonebook.contacts.items;
-export const selectIsLoading = state => state.phonebook.contacts.isLoading;
-export const selectError = state => state.phonebook.contacts.error;
-export const selectFilter = state => state.phonebook.filter;
-export const getFilteredContacts = createSelector([selectContacts, selectFilter], (items, filter) => {
-    return items.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase().trim()))
-});
